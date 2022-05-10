01dbdf4a9117f0068e6a9bc41e9ecb9989191461
Taylor Huffman <taylor@crucialartists.com>
2022-05-10 12:23:13 -0500

    commit tree 738caa38852523ec35a9d585769ba6d4e8f35144

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,26 @@
 class Triangle
-  # write code here
+  
+  def initialize(length1, length2, length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+  end
+
+  def kind
+    @length1, @length2, @length3 = [@length1, @length2, @length3].sort
+    if @length1 <= 0 || @length1 + @length2 <= @length3
+      raise TriangleError
+    elsif ((@length1 == @length2) && (@length1 == @length3) && (@length2 == @length3))
+      :equilateral
+    elsif ((@length1 == @length2) || (@length1 == @length3) || (@length2 == @length3))
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+
+  end
+
 end
